25274e6da27f3927eaa2b48c4229dd96b7398560
Cremattsd <110553037+Cremattsd@users.noreply.github.com>
2025-03-25 17:20:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="real_nex_sync_api_data_facade",
     version="1.0.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="src/real_nex_sync_api_data_facade"),  # Pointing to the correct directory
+    package_dir={"": "src/real_nex_sync_api_data_facade"},  # Correct path
     install_requires=[
         "requests",  # Add any dependencies your SDK needs
     ],
